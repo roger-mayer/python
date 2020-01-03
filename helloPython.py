fe26a3749d5e459d1f52790d2e4e32b59f0e0e4e
Roger Mayer <rmayer1984@gmail.com>
2020-01-03 09:51:42 -0600

    modified helloPython

--- helloPython.py
+++ helloPython.py
@@ -1,2 +1,46 @@
+# comment
 print("hello world")
 
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+x = "Python is "
+y = "awesome"
+z =  x + y
+print(z)
+
+"""If you create a variable with the same name inside
+a function, this variable will be local,
+and can only be used inside the function.
+The global variable with the same name will remain as it was,
+global and with the original value."""
+x = "awesome"
+def myfunc():
+    x = "fantastic"
+    print("Python is " + x)
+myfunc()
+print("Python is " + x)
+"""There are three numeric types in Python:
+
+int
+float
+complex
+Variables of numeric types are created when you assign a value to them:
+
+Example"""
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+"""To verify the type of any object in Python, use the type() function:
+
+Example"""
+print(type(x))
+print(type(y))
+print(type(z))
