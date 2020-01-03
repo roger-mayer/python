cb3113c754bd5be3d629ffc1963393ccad8cb342
Roger Mayer <rmayer1984@gmail.com>
2020-01-03 10:08:04 -0600

    added python fizzbuzz

--- helloPython.py
+++ helloPython.py
@@ -44,3 +44,15 @@ Example"""
 print(type(x))
 print(type(y))
 print(type(z))
+
+for fizzbuzz in range(100):
+    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+        print("fizzbuzz")
+        continue
+    elif fizzbuzz % 3 == 0:
+        print("fizz")
+        continue
+    elif fizzbuzz % 5 == 0:
+        print("buzz")
+        continue
+    print(fizzbuzz)
